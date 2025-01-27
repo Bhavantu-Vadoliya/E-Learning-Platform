f8133bf2d0ee9b14d0136073df13127f9c5cb877
Bhavantu <bhavantu04@gmail.com>
2025-01-27 22:25:49 +0530

    Delete aiutor directory

--- aiutor/aiutorapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AiutorappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'aiutorapp'
--- aiutor/aiutorapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'index.html')
-
-def home(request):
-    return render(request, 'web/home.html')
